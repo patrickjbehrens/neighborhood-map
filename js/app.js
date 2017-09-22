c9a50f7081dae070e90dbc796a3deabe362709b6
Patrick Behrens <patrickjbehrens@gmail.com>
2017-09-22 14:26:45 -0700

    Filtering, animation, infowindow

--- js/app.js
+++ js/app.js
@@ -35,7 +35,7 @@ var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 37.879789, lng: -122.289945},
-    zoom: 14,
+    zoom: 13,
     mapTypeControl: false
   });
 
@@ -52,74 +52,108 @@ var ViewModel = function() {
   // through the Location object.
   this.locationList = ko.observableArray([]);
 
+  // Initializes the infoWindow.
+  var infoWindow = new google.maps.InfoWindow();
+
+  // Creates the default marker.
+  var defaultIcon = makeMarkerIcon('FF3F33');
+
+  // Creates the selected location marker.
+  var selectedIcon = makeMarkerIcon('33FF49');
+
   initLocations.forEach(function(locationItem){
     var marker = new google.maps.Marker({
       position: locationItem.position,
       title: locationItem.title,
+      icon: defaultIcon,
       map: map
     });
     locationItem.marker = marker;
+    locationItem.marker.addListener('click', function() {
+      setMarkersDefault();
+      populateInfoWindow(this, infoWindow);
+    });
     self.locationList.push(new Location(locationItem));
   });
 
-  // this.currentLocation = ko.observable(this.locationList()[0]); // Need this?
+  // Initializes the currentLocation binding purposes.
+  this.currentLocation = ko.observable(this.locationList()[0]);
 
   // Builds filtered lists and markers based on user input.
   self.selectedCategory = ko.observable('all');
 
   self.filteredLocations = ko.computed(function() {
-    self.locationList().forEach(function(filterItem){
-      var category = self.selectedCategory();
-      console.log(filterItem);
-      console.log(category);
-      if(category === 'all') {
-        return filterItem;
-        filterItem.marker.setVisible(true);
-      } else if(filterItem.category() === category) {
-          return filterItem;
-          filterItem.marker.setVisible(true);
-      } else {
-          filterItem.marker.setVisible(false);
-      }
+    var selectedCategory = self.selectedCategory().toLowerCase();
+
+    // If the 'All' category is selected, shows all listings and markers.
+    if (selectedCategory === 'all') {
+      self.locationList().forEach(function(location) {
+        if (location.marker) {
+          location.marker.setVisible(true);
+        }
+      });
+      return self.locationList();
+    }
+
+    // Else if a category is selected, filters the list and markers.
+    return ko.utils.arrayFilter(self.locationList(), function(location) {
+      var category = location.category().toLowerCase();
+      var match = category === selectedCategory; // true or false
+      // toggle marker visibility
+      self.locationList().forEach(function(location) {
+        if (location.category() === selectedCategory) {
+          location.marker.setVisible(true);
+        } else {
+          location.marker.setVisible(false);
+        }
+      })
+      return match;
     });
   });
 
-  // self.filteredLocations = ko.computed(function() {
-  //   switch (self.selectedCategory()) {
-  //
-  //   default:
-  //     return self.locationList();
-  //
-  //   case 'groomer':
-  //     var tempList = self.locationList().slice();
-  //     return tempList.filter(function(filterItem) {
-  //       return filterItem.category() === 'groomer';
-  //     });
-  //
-  //   case 'park':
-  //     var tempList = self.locationList().slice();
-  //     return tempList.filter(function(filterItem) {
-  //       return filterItem.category() === 'park';
-  //     });
-  //
-  //   case 'store':
-  //     var tempList = self.locationList().slice();
-  //     return tempList.filter(function(filterItem) {
-  //       return filterItem.category() === 'store';
-  //     });
-  //   }
-  // });
 
-  // self.filteredLocations = ko.computed(function() {
-  //   var category = self.selectedCategory();
-  //   if(category === 'all') {
-  //     return self.locationList();
-  //   } else {
-  //     var tempList = self.locationList().slice();
-  //     return tempList.filter(function(location) {
-  //       return location.category() === category;
-  //     });
-  //   }
-  // });
+  // Creates markers, passing in a color to toggle between active and inactive markers.
+  function makeMarkerIcon(markerColor) {
+    var markerImage = new google.maps.MarkerImage(
+      'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+      '|40|_|%E2%80%A2',
+      new google.maps.Size(21, 34),
+      new google.maps.Point(0, 0),
+      new google.maps.Point(10, 34),
+      new google.maps.Size(21,34));
+    return markerImage;
+  }
+
+  function setMarkersDefault() {
+    self.locationList().forEach(function(locationItem) {
+      locationItem.marker.setIcon(defaultIcon);
+    });
+  };
+
+  // Populates the infoWindow with location content.
+  function populateInfoWindow(marker, infowindow) {
+
+    if (infowindow.marker != marker) {
+         // Clear the infowindow content to give the streetview time to load.
+         infowindow.setContent('');
+         infowindow.marker = marker;
+         // Make sure the marker property is cleared if the infowindow is closed.
+         infowindow.addListener('closeclick', function() {
+             marker.setIcon(defaultIcon);
+         });
+     }
+
+    marker.setIcon(selectedIcon);
+    infowindow.setContent('<div>' + marker.title + '</div><div></div>');
+    infowindow.open(map, marker);
+  };
+
+  // Connects the selected location from the list to it's marker.
+  this.currentLocation = function(location) {
+    google.maps.event.trigger(this.marker, 'click');
+  };
+
+
+
 
 };
